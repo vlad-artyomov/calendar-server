11d436ba949bda17e1a5f2f2d4b7dbbfdae06061
Vlad Artyomov <artyomov.dev@gmail.com>
2017-03-06 10:55:05 +0300

    Remove unnecessary comments.

--- src/main/java/com/artyomov/ServerApplication.java
+++ src/main/java/com/artyomov/ServerApplication.java
@@ -4,12 +4,11 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.ws.rs.ApplicationPath;
 
-//TODO refactor this javadoc
-
 /**
- * Created by Vlad on 06.03.2017.
+ * @author Vlad Artyomov
+ * @since 06.03.2017, 10:49
  */
-@ApplicationPath("/*")
+@ApplicationPath("/")
 public class ServerApplication extends ResourceConfig {
     public ServerApplication() {
         packages("com.artyomov.resources");
