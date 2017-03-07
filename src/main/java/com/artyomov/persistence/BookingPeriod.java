8076f55a2f6bd3715fdd3c395d1cb4b65e8df95b
Vlad Artyomov <artyomov.dev@gmail.com>
2017-03-07 18:55:00 +0300

    Create BookingPeriod class.

---
+++ src/main/java/com/artyomov/persistence/BookingPeriod.java
@@ -0,0 +1,9 @@
+package com.artyomov.persistence;
+
+/**
+ * @author <a href="mailto:artyomov.dev@gmail.com">Vlad Artyomov</a>
+ *         Date: 07.03.17
+ *         Time: 17:38
+ */
+public class BookingPeriod {
+}
